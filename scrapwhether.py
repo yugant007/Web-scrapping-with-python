e76d594910f1afb34f236b92b0cff14e8a2bed41
yugant bansal <myself.yugant@gmail.com>
2018-03-04 00:35:41 +0530

    scrapwhether.py

---
+++ scrapwhether.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+import requests
+
+from bs4 import BeautifulSoup
+
+page=requests.get("https://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168#.WprSqqiWbIU")
+
+soup=BeautifulSoup(page.content,"html.parser")
+
+seven_day=soup.find(id="seven-day-forecast")
+
+period_tags=seven_day.select(".tombstone-container .period-name")
+
+periods=[pt.get_text() for pt in period_tags]
+
+short_descs=[sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+
+temps=[t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+
+descs=[d["title"] for d in seven_day.select(".tombstone-container img")]
+
+whether=pd.DataFrame({
+    
+      "period":periods,
+      
+      "short_desc":short_descs,
+      
+      "temp":temps,
+      
+      "desc":descs
+      
+    })
+
+print(whether)
